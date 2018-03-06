c8182c0410824bcd3cd7fa49eb832ffee490df76
RossPin <dorpetz@hotmail.com>
2018-03-06 15:39:16 +1300

    make function checkForWin

--- minesweeper.js
+++ minesweeper.js
@@ -2,14 +2,16 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
 var board = {cells: []}
-  
+
+// add cells to board.cells  
 var gridSize = 3;
 for (var i=0; i < gridSize; i++) {
   for (var j=0; j < gridSize; j++){
-    board.cells.push({row: i, col: j, isMine: false, isMarked: false, hidden: true})
+    board.cells.push({row: i, col: j, isMine: false, hidden: true})
   }
 }
-//
+
+// define cells as mines
 board.cells[3].isMine = true
 board.cells[8].isMine = true
 console.log(board.cells)
@@ -20,17 +22,24 @@ function startGame () {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
   }
   lib.initBoard()
+  document.addEventListener('click', checkForWin)
+  document.addEventListener('contextmenu', checkForWin)
 }
 
 // Define this function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
-function checkForWin () {
-
+function checkForWin () {  
+  for (i = 0; i < board.cells.length; i++) {
+    //if cell is mine and not marked exit function
+    if (board.cells[i].isMine && !board.cells[i].isMarked) return
+    //if cell is not mine and still hidden exit function
+    if (!board.cells[i].isMine && board.cells[i].hidden) return
+  } 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+  lib.displayMessage('You win!')
 }
 
 // Define this function to count the number of mines around the cell
